65e65f525df3b639bd350be928a77f8bd1220a09
magnuns17 <131685897+magnuns17@users.noreply.github.com>
2024-02-29 00:43:14 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const menuItem = document.querySelectorAll('.nav-links')
 
-function selectLink(){
+function selectLink() {
     menuItem.forEach((item) =>
-    item.classList.remove('active'))
+        item.classList.remove('active'))
 
     this.classList.add('active')
 }
 
-menuItem.forEach((item)=>
- item.addEventListener('click', selectLink)
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
 
 )
 
 const btnExp = document.querySelector('.toggle-menu')
 const menuSide = document.querySelector('.container-menu')
 
-btnExp.addEventListener('click', function() {
-    menuSide.classList.toggle('expandir') 
+btnExp.addEventListener('click', function () {
+    menuSide.classList.toggle('expandir')
 });
 
-window.sr = ScrollReveal({ reset: true});
+window.sr = ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.area-1', {
     delay: 700,
-  
- 
-             
+
+
+
 });
 
 ScrollReveal().reveal('.area-2', {
@@ -35,7 +35,7 @@ ScrollReveal().reveal('.area-2', {
         y: 100,
         z: 0,
     }
-             
+
 });
 
 let sec = document.querySelectorAll('section')
@@ -45,15 +45,15 @@ let navLinks = document.querySelectorAll('.nav-links')
 
 window.onscroll = () => {
     sec.forEach(section => {
-        let top = window.scrollY +210 ;
-        let offset = section.offsetTop -150;
-        let height = section.offsetHeight +300 ;
+        let top = window.scrollY + 280;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight + 300;
         let id = section.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-              document.querySelector('.nav-links[href*=' + id + ']').classList.add('active');
+                document.querySelector('.nav-links[href*=' + id + ']').classList.add('active');
 
             })
         }
